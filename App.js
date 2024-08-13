3976f4f4d7d91e2cd4f5b3cf98faf1cd837cdab3
Luis Javier Leyva Bustamante <luisleyva.bq@gmail.com>
2024-08-12 18:17:07 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [number, onChangeNumber] = useState('');
+  const { onPress, title = 'Convert' } = useState();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={styles.title}>Celcius to Farenheit converter</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Celcius"
+        keyboardType="numeric"
+      />
+      <Text>{number}</Text>
+      <View style={styles.container}>
+        <Pressable style={styles.button} onPress={onPress}>
+          <Text stlye={styles.buttonText}>{title}</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +31,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10
+  },
+  title: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
